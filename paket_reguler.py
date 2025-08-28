813fbcd9edd2008aebf6941ee34ab6ba5d0ffa30
rif97324 <rif97324@gmail.com>
2025-08-28 19:21:48 +0700

    Update paket_reguler.py

--- paket_reguler.py
+++ paket_reguler.py
@@ -3,14 +3,14 @@ from api_request import send_api_request, get_family
 
 PACKAGE_FAMILY_CODE = "6fda76ee-e789-4897-89fb-9114da47b805"
 
-def get_package_reguler(api_key: str, tokens: dict):
+def get_package_xut(api_key: str, tokens: dict):
     packages = []
     
     data = get_family(api_key, tokens, PACKAGE_FAMILY_CODE)
     package_variants = data["package_variants"]
     start_number = 1
     for variant in package_variants:
-        if variant["name"] == "For Reguler":
+        if True:
             for option in variant["package_options"]:
                 if True:
                     friendly_name = option["name"]
