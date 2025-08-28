97046bdb1676e2625723176aed83fbb78ff8eed7
rif97324 <rif97324@gmail.com>
2025-08-29 06:12:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from util import load_token, ensure_api_key
 from paket_xut import get_package_xut
 from paket_reguler import get_package_reguler
 from paket_flex import get_package_flex
-from paket_45gb import get_package_45gb
+from paket_bundling import get_package_bundling
 from my_package import fetch_my_packages
 
 user_data = {
@@ -63,8 +63,8 @@ def main():
                 
                 show_package_menu(api_key, user_data["tokens"], packages)
             elif choice == "6":
-                # 45gb
-                packages = get_package_45gb(api_key, user_data["tokens"])
+                # Bundling
+                packages = get_package_bundling(api_key, user_data["tokens"])
                 
                 show_package_menu(api_key, user_data["tokens"], packages)
             elif choice == "99":
